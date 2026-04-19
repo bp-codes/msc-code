90e03f5a010ae125c3b19619b192c13775606c55
Ben <benalmer1983@gmail.com>
2026-04-19 15:40:01 +0100

    updated trial_004

--- trials/trial_004_stopping_power/build_cpu.sh
+++ trials/trial_004_stopping_power/build_cpu.sh
@@ -4,18 +4,21 @@ set -Eeuo pipefail
 trap 'echo "Error on line $LINENO (exit code $?)" >&2' ERR
 
 {
-
+    # Precise (128bit)
     g++ -O3 -ffast-math -march=native -std=c++23 -fext-numeric-literals src/precise.cpp -lquadmath -o bin/precise.x
 
+    # 64 Bit
     g++ -O3 -ffast-math -march=native -std=c++23 src/serial.cpp -o bin/serial.x
-    g++ -O3 -ffast-math -march=native -std=c++23 src/parallel_transform.cpp -ltbb -o bin/parallel_transform.x
+    g++ -O3 -ffast-math -march=native -std=c++23 src/serial_stl_transform.cpp -o bin/serial_stl_transform.x -ltbb
+    g++ -O3 -ffast-math -march=native -std=c++23 src/parallel_stl_transform.cpp -o bin/parallel_stl_transform.x -ltbb 
     g++ -O3 -ffast-math -march=native -std=c++23 src/parallel_thread.cpp -o bin/parallel_thread.x
     g++ -O3 -ffast-math -march=native -std=c++23 -fopenmp src/parallel_openmp.cpp -o bin/parallel_openmp.x
     g++ -O3 -ffast-math -march=native -std=c++23 -fopenmp src/parallel_openmp_simd.cpp -o bin/parallel_openmp_simd.x
 
-
+    # 32 Bit
     g++ -O3 -ffast-math -march=native -std=c++23 src/serial_32.cpp -o bin/serial_32.x
-    g++ -O3 -ffast-math -march=native -std=c++23 src/parallel_transform_32.cpp -ltbb -o bin/parallel_transform_32.x
+    g++ -O3 -ffast-math -march=native -std=c++23 src/serial_stl_transform_32.cpp -o bin/serial_stl_transform_32.x -ltbb
+    g++ -O3 -ffast-math -march=native -std=c++23 src/parallel_stl_transform_32.cpp -ltbb -o bin/parallel_stl_transform_32.x
     g++ -O3 -ffast-math -march=native -std=c++23 src/parallel_thread_32.cpp -o bin/parallel_thread_32.x
     g++ -O3 -ffast-math -march=native -std=c++23 -fopenmp src/parallel_openmp_32.cpp -o bin/parallel_openmp_32.x
     g++ -O3 -ffast-math -march=native -std=c++23 -fopenmp src/parallel_openmp_simd_32.cpp -o bin/parallel_openmp_simd_32.x
--- trials/trial_004_stopping_power/run_cpu.sh
+++ trials/trial_004_stopping_power/run_cpu.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 set -Eeuo pipefail
+export NUM_THREADS=6
+mkdir -p results
+
+RUNS=3
+RUN_TIME=10.0
+VECTOR_SIZE=1000000
 
 trap 'echo "Error on line $LINENO (exit code $?)" >&2' ERR
 
 {
-    ./bin/precise.x 5.0 1000000
+    ./bin/precise.x "$RUN_TIME" "$VECTOR_SIZE"
 
-    RUNS=5
     export NUM_THREADS=6
 
     for ((i=1; i<=RUNS; i++))
     do
         echo "  Run $i"
 
-        /usr/bin/time -v -- ./bin/serial.x 5.0 1000000
-        /usr/bin/time -v -- ./bin/parallel_transform.x 5.0 1000000
-        /usr/bin/time -v -- ./bin/parallel_thread.x 5.0 1000000
-        /usr/bin/time -v -- ./bin/parallel_openmp.x 5.0 1000000
-        /usr/bin/time -v -- ./bin/parallel_openmp_simd.x 5.0 1000000
+        /usr/bin/time -v -- ./bin/serial.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/serial_stl.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/parallel_transform.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/parallel_stl.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/parallel_thread.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/parallel_openmp.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/parallel_openmp_simd.x "$RUN_TIME" "$VECTOR_SIZE"
 
 
-        /usr/bin/time -v -- ./bin/serial_32.x 5.0 1000000
-        /usr/bin/time -v -- ./bin/parallel_transform_32.x 5.0 1000000
-        /usr/bin/time -v -- ./bin/parallel_thread_32.x 5.0 1000000
-        /usr/bin/time -v -- ./bin/parallel_openmp_32.x 5.0 1000000
-        /usr/bin/time -v -- ./bin/parallel_openmp_simd_32.x 5.0 1000000
+        /usr/bin/time -v -- ./bin/serial_32.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/parallel_transform_32.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/parallel_thread_32.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/parallel_openmp_32.x "$RUN_TIME" "$VECTOR_SIZE"
+        /usr/bin/time -v -- ./bin/parallel_openmp_simd_32.x "$RUN_TIME" "$VECTOR_SIZE"
 
     done
 
